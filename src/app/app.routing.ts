d56030ff5f45684d206871b5729b23b552580533
EthanZhang <ethan.zhang@orig3n.com>
2018-06-08 16:38:44 -0400

    Added auth-guard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MaterialDashboardComponent } from './components/material-dashboard/material-dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 export const APP_ROUTES: Routes = [
-    { path: 'home', component: MaterialDashboardComponent},
+    { path: 'home', component: MaterialDashboardComponent, canActivate: [AuthGuardService]},
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
-    { path: '', redirectTo:'/home', pathMatch:'full' },
+    { path: '', redirectTo:'/login', pathMatch:'full' },
 ];
 
 export const APP_ROUTING_MODULE = RouterModule.forRoot(APP_ROUTES);
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { JWT_AUTH_TOKEN } from '../models/app-constants';
+import {JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private jwtHelperService: JwtHelperService) { }
   
   getToken() {
     return localStorage.getItem(JWT_AUTH_TOKEN);
@@ -17,6 +18,12 @@ export class AuthService {
       localStorage.setItem(JWT_AUTH_TOKEN, token);
     }
   }
+
+  isAuthenticated(): boolean {
+    const token = this.getToken();
+    return !this.jwtHelperService.isTokenExpired(token);
+  }
+
 
   isLoggedIn() { }
 }
